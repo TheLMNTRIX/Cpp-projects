653b5786dfd79c42a2a9c83ccf0c41b7f2fdc394
TheLMNTRIX <90512241+TheLMNTRIX@users.noreply.github.com>
2022-03-09 16:48:53 +0530

    Create Voting Eligibility.cpp

---
+++ Voting Eligibility.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+	int age;
+	cout << "Enter your age: ";
+	cin >> age;
+
+	if (age >= 18)
+	{
+		cout << "You are eligible to vote.";
+	}
+	else
+	{
+		cout << "You are not eligible to vote, please wait " << 18 - age << " years to vote.";
+	}
+	
+	return 0;
+}
